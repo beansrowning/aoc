7af05de14f2145b7b2329921f08c9896378a11df
Sean Browning <25888380+beansrowning@users.noreply.github.com>
2023-12-04 17:06:30 -0500

    LOL ok, so I just did the second day first

---
+++ 02/solution.R
@@ -0,0 +1,55 @@
+# === Load data ==============================================
+data_pull <- function(path = "01/data") {
+  con <- file(path, "r")
+  on.exit(close(con))
+
+  out <- readLines(con)
+
+  return(out)
+}
+
+raw_data <- data_pull()
+
+# === Lookup table ===========================================
+numbers <- c(
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine"
+)
+
+numbers_lk <- setNames(as.character(1:9), numbers)
+
+# === function ================================
+get_number <- function(patterns, strs) {
+  out <- integer(length(strs))
+
+  inner_fx <- function(pattern, str) {
+    out <- gregexpr(pattern, str)
+    out[[1]][out[[1]] == -1] <- NA_integer_
+    return(out[[1]])
+  }
+
+  for (i in seq_along(strs)) {
+    # Determine first number
+    reg_results <- lapply(patterns, inner_fx, strs[i])
+
+    first_digit <- which.min(vapply(reg_results, min, integer(1)))
+    last_digit <- which.max(vapply(reg_results, max, integer(1)))
+
+    out[i] <- as.integer(paste(first_digit, last_digit, sep = ""))
+  }
+
+  return(out)
+}
+
+# === check =================
+clean_data <- get_number(paste(names(numbers_lk), numbers_lk, sep = "|"), raw_data)
+
+sum(clean_data)
+# 54980
