2edf7d284b84e822853e8348d4a4b0b7f821866f
Sean Browning <25888380+beansrowning@users.noreply.github.com>
2023-12-04 17:03:10 -0500

    LOL much easier than I was making it

--- 01/solution.R
+++ 01/solution.R
@@ -26,21 +26,14 @@ numbers <- c(
 numbers_lk <- setNames(as.character(1:9), numbers)
 
 # === function ================================
-get_number <- function(patterns, strs) {
+get_number <- function(strs) {
   out <- integer(length(strs))
 
-  inner_fx <- function(pattern, str) {
-    out <- gregexpr(pattern, str)
-    out[[1]][out[[1]] == -1] <- NA_integer_
-    return(out[[1]])
-  }
-
+  reg_results <- gregexec("\\d{1}", strs)
   for (i in seq_along(strs)) {
-    # Determine first number
-    reg_results <- lapply(patterns, inner_fx, strs[i])
 
-    first_digit <- which.min(vapply(reg_results, min, integer(1)))
-    last_digit <- which.max(vapply(reg_results, max, integer(1)))
+    first_digit <- substr(strs[i], min(reg_results[[i]]), min(reg_results[[i]]))
+    last_digit <- substr(strs[i], max(reg_results[[i]]), max(reg_results[[i]]))
 
     out[i] <- as.integer(paste(first_digit, last_digit, sep = ""))
   }
@@ -49,10 +42,7 @@ get_number <- function(patterns, strs) {
 }
 
 # === check =================
-clean_data <- get_number(paste(names(numbers_lk), numbers_lk, sep = "|"), raw_data)
+clean_data <- get_number(raw_data)
 
 sum(clean_data)
-# 54980 # Wrong
-
-check_nums <- sample(seq_along(clean_data), 20)
-setNames(clean_data[check_nums], raw_data[check_nums])
+# 55816 # Correct
