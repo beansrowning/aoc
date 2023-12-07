52eaf6d935c3dbb61b65525f032585bc8038fabf
Sean Browning <25888380+beansrowning@users.noreply.github.com>
2023-12-07 17:28:35 -0500

    First stab at day 7 part 1

---
+++ 07/part_1.R
@@ -0,0 +1,82 @@
+# === Load data ==============================================
+data_pull <- function(lookup, path = "07/data") {
+  con <- file(path, "r")
+  on.exit(close(con))
+
+  raw_text <- readLines(con)
+
+  out <- strsplit(raw_text, " ") |>
+      lapply(
+        function(x){
+        setNames(
+          list(
+            lookup[strsplit(x[1], "")[[1]]],
+            x[2]
+          ),
+          c("hand", "bet")
+        )
+        }
+      )
+
+
+
+  return(out)
+}
+
+# Card lookup
+num_lk <- c(
+  setNames(2:9, as.character(2:9)),
+  "T" = 10,
+  "J" = 11,
+  "Q" = 12,
+  "K" = 13,
+  "A" = 14
+)
+
+# Pull data, coded how we want
+parsed_data <- data_pull(num_lk)
+
+winnings <- function(hand) {
+  distinct_n <- length(unique(hand))
+
+  # Four-of-a-kind if 1 (individual card) or 4 (one of the 4)
+  # Full house if 2 (one of the pair) or 3 (one of the 3-of-a-kind)
+  four_distinct_case <- \(x) switch(length(x[x == x[1]]), 4, 3, 3, 4)
+  three_distinct_case <- \(x) c(2L, 3L)[any(tabulate(x) == 3) + 1]
+
+  switch(
+    distinct_n,
+    5, # 1 distinct number has to be 5-of-a-kind
+    four_distinct_case(hand), # disaggregate four-of-a-kind + 1 or full house
+    three_distinct_case(hand),
+    2, # Must be exactly 1 pair
+    1 # high card
+  )
+}
+
+
+winnings_mat <- cbind(
+  idx = seq_along(parsed_data),
+  base_win = vapply(parsed_data, \(x) winnings(x[["hand"]]), double(1)),
+  bet = vapply(parsed_data, \(x) as.numeric(x[["bet"]]), double(1))
+)
+
+card_mat <- t(vapply(parsed_data, \(x) x[["hand"]], double(5)))
+colnames(card_mat) <- sprintf("card_%d", 1:5)
+
+full_mat <- cbind(winnings_mat, card_mat)
+
+full_mat_sort <- full_mat[
+  order(
+    full_mat[, "base_win"],
+    full_mat[, "card_1"],
+    full_mat[, "card_2"],
+    full_mat[, "card_3"],
+    full_mat[, "card_4"],
+    full_mat[, "card_5"],
+    decreasing = TRUE
+  ),
+]
+
+total_winnings <- sum(full_mat_sort[, "bet"] * rev(seq_len(nrow(full_mat_sort))))
+# 249134802 # wrong
