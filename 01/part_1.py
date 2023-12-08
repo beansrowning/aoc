fa9e40db42cae5aa5df610c5ca6cefda07ab8d85
Sean Browning <25888380+beansrowning@users.noreply.github.com>
2023-12-08 15:35:21 -0500

    cleaner approach

--- 01/part_1.py
+++ 01/part_1.py
@@ -10,8 +10,7 @@ def main():
         raw_data = f.read().splitlines()
     
     all_nums = [re.findall(number_re, st) for st in raw_data]
-    filtered_nums = [[num for i, num in enumerate(num_list) if i in [0, len(num_list) - 1]] for num_list in all_nums]
-    out = [int("".join(num_list * 2)) if len(num_list) == 1 else int("".join(num_list)) for num_list in filtered_nums]
+    out = [int("".join([num_list[0], num_list[-1]])) for num_list in all_nums]
 
     print(sum(out))
     # 55816
