902c85a519175fda29a6dd23e7f1c68a2b2fe87d
Sean Browning <25888380+beansrowning@users.noreply.github.com>
2023-12-13 17:10:59 -0500

    Better Day 1 pt 1 solution too

--- 01/part_1.R
+++ 01/part_1.R
@@ -10,33 +10,10 @@ data_pull <- function(path = "01/data") {
 
 raw_data <- data_pull()
 
-# === Lookup table ===========================================
-numbers <- c(
-  "one",
-  "two",
-  "three",
-  "four",
-  "five",
-  "six",
-  "seven",
-  "eight",
-  "nine"
-)
-
-numbers_lk <- setNames(as.character(1:9), numbers)
-
 # === function ================================
 get_number <- function(strs) {
-  out <- integer(length(strs))
-
-  reg_results <- gregexec("\\d{1}", strs)
-  for (i in seq_along(strs)) {
-
-    first_digit <- substr(strs[i], min(reg_results[[i]]), min(reg_results[[i]]))
-    last_digit <- substr(strs[i], max(reg_results[[i]]), max(reg_results[[i]]))
-
-    out[i] <- as.integer(paste(first_digit, last_digit, sep = ""))
-  }
+  reg_results <- regmatches(strs, gregexec("\\d{1}", strs))
+  out <- vapply(reg_results, \(x) (10 * as.double(x[1])) + as.double(x[length(x)]), double(1))
 
   return(out)
 }
@@ -45,4 +22,4 @@ get_number <- function(strs) {
 clean_data <- get_number(raw_data)
 
 sum(clean_data)
-# 55816 # Correct
+# 55816
